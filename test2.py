8f7217ee99b235bfeba083ef7ba29810e8e0dd44
sunil <sunil@codebee.dk>
2020-07-22 10:04:18 +0545

    all football play pyplot

--- test2.py
+++ test2.py
@@ -21,3 +21,16 @@ list_of_num = [1,10,3,14,5,6,10]
 print(find_Value(list_of_num,10))
         
     
+from matplotlib import pyplot as py
+
+sitename=["allfootball","nepalfreelance","school","ismartmandu",'mysql','swscfa']
+memory=[3.15,1.63,0.77,0.70,0.54,0.41]
+#labels=['science','Math','Account']
+py.plot(sitename,memory)
+py.xlabel("Website Name")
+py.ylabel("Memory allocated in GB")
+py.title("Usage of Total Memory (9GB) for different site")
+py.text('allfootballplay',3.15,'high allocated memory')
+py.grid(True)
+# py.xticks[[80,75,60,55,75,65,55,80],['Science','Math','Nepali','English','Computer','Opt','Health','Account']]
+py.show()
